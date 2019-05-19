cb075947e482534f9a86c3eece92db88f693fa35
KamaronB <kamaron.bickham@gmail.com>
2019-05-18 22:51:08 -0500

    working on models

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+ @receiver(post_save, sender=User)
+ def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+ @receiver(post_save, sender=User)
+ def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+
+
+
+class Relationship(models.Model):
+    type = models.TextField(max_length=50,blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.Cascade)
+
+
+class People(models.Model):
+    friend_id=PositiveIntegerField(null=True, blank=True)
