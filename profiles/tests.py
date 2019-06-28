df89c74f818d20ebc40df498d0879a8d42307dbb
KamaronB <Kamaron.bickham@gmail.com>
2019-06-28 09:15:57 -0500

    no styling

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Profile, Relationship, People, requests,Message, Room
 # Create your tests here.
+
+
+
+class ProfileTestCase(TestCase):
+
+    def createUser(self):
+        mcFakins= User.objects.create(first_name="fake", last_name="User" username="mcFakins")
+        mcFakins= User.objects.create(first_name="fake", last_name="User" username="mcFakins")
+
+        # mckFakins.profile.bio=
+
+
+    def verify_Profiles(self):
